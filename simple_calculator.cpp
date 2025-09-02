02c0a89a16318f36fdb47f6ac26b20bb955fedaf
Jesus sanchez <Jsanchez@truffulaclub.org>
2025-09-02 13:02:15 -0500

    Initial commit: Add simple calculator project

---
+++ simple_calculator.cpp
@@ -0,0 +1,42 @@
+/*this simple calculator program should:
+- Ask the user to enter two numbers.
+- Ask the user to enter an arithmetic operator (+, -, *, /)
+- Perform the operation and display the results.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter a second number: ";
+    cin >> num2;
+
+    if (op == '+')
+        result = num1 + num2;
+
+        else if (op == '-')
+        result = num1 - num2;
+
+        else if (op == '*')
+        result = num1 * num2;
+
+        else if (op == '/')
+        result = num1 / num2;
+
+        else {
+            cout << "invalid operator!" << endl;
+            return 1;
+        }
+
+        cout << "Result: " << result << endl;
+
+
+    return 0;
+}
